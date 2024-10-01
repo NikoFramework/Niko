ddf8d7f3f387fa33ff801f1133842684c03e02fc
kamenomi-dev <2937396379@qq.com>
2024-10-02 01:07:25 +0800

    take a rest, 这个代码的运行顺序不太对

--- core/connection.ts
+++ core/connection.ts
@@ -1,47 +1,11 @@
+export interface AdapterInterface {}
 
+export abstract class Adapter {}
 
-import { type Logger } from "winston";
-
-export interface ConnectionOptions {
-  provider: OneAndMore<Connection.ServiceProvider>
-}
-
-export class Connection<T extends Connection.Types> {
-  private type: T;
-  private provider: Connection.ServiceProvider;
-
-  public static $logger: Logger;
-  public static $config: NonNullable<typeof generalConfig.Config.connection>;
-
-  public constructor(options: ConnectionOptions) {
-    Connection.$logger = logger.child({ modules: ["module", "logger"] });
-    Connection.$config = generalConfig.Config.connection!;
+class Connection {
+  public constructor() {
     
-    if (!Connection.$config) {
-      Connection.$logger.error("Incorrect initialization! Config module haven't inited yet!");
-    }
   }
 }
 
-export namespace Connection {
-  export enum Types {
-    Forward, Reverse, Http, Other
-  }
-
-  export abstract class ServiceProvider {
-    public readonly type: symbol;
-
-    public constructor(private _initialType: Types) {
-      this["type"] = Symbol(_initialType);
-    }
-  }
-
-  export namespace Providers {
-    class WebsocketForward extends ServiceProvider {
-      public constructor() {
-        super(Types.Forward);
-      }
-    }
-  }
-}
-
+export default new Connection();
